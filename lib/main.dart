30a24014067a4d453bb734258069ae30a5ffcf88
Sanchit Dass <sanchitd9@gmail.com>
2024-06-18 14:27:07 -0400

    Add provider for dummyMeals

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:meals/screens/tabs.dart';
 
@@ -12,7 +13,11 @@ final theme = ThemeData(
 );
 
 void main() {
-  runApp(const App());
+  runApp(
+    const ProviderScope(
+      child: const App(),
+    ),
+  );
 }
 
 class App extends StatelessWidget {
@@ -21,8 +26,8 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: theme,
-        home: const TabsScreen(),
+      theme: theme,
+      home: const TabsScreen(),
     );
   }
 }
