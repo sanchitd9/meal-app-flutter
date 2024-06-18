fb4253e38e857d755fefbe52e19f035e84f1d744
Sanchit Dass <sanchitd9@gmail.com>
2024-06-18 19:58:13 -0400

    Use provider to change the favorite icon dynamically

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:meals/providers/favorites_provider.dart';
-import 'package:meals/providers/meals_provider.dart';
 import 'package:meals/screens/categories.dart';
 import 'package:meals/screens/filters.dart';
 import 'package:meals/screens/meals.dart';
